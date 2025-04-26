354dda3dc82b824983e3451521153ee5de30ab7b
DaredemoDaisukiRobot <128045170+DaredemoDaisukiRobot@users.noreply.github.com>
2025-04-26 15:22:42 +0800

    Delete _coder directory

--- _coder/custom_model.py
+++
@@ -1,9 +0,0 @@
-def plus(a, b):
-    res1 = a + b
-    print(res1)
-
-def minus(x, y):
-    res2 = x - y
-    print(res2)
-
-print("你好~~")
--- _coder/spam.py
+++
@@ -1,3 +0,0 @@
-# coding: utf-8
-# 在 ipython kernel 上運行
-get_ipython().run_line_magic('save', 'spam.py 1-3 7 9')
